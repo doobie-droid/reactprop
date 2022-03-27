f4abd2e4fd23aeea7af3ae6257cce28aebbfaaab
Leslie Douglas <lesliedouglas23@gmail.com>
2022-03-27 12:30:16 +0100

    yah

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import Card from "./card";
 import contacts from "../contacts";
 console.log(contacts);
+//you must put the key prop
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      number={contact.phone}
+      email={contact.email}
+      img={contact.imgURL}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
       <Card
         img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
         name="beyonce"
